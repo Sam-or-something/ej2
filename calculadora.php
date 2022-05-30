ccacbc387f9f286cc5a86bd2ed004581ad6bf415
Sam <89226291+Sam-or-something@users.noreply.github.com>
2022-05-30 15:19:42 -0300

    Initial commit

---
+++ calculadora.php
@@ -0,0 +1,24 @@
+<?php 
+
+$danios = array("golpe" => 10, "patada" => 20, "espadazo" => 50);
+
+function calcularDanio($vida, $ataque, $pocion){
+    if($vida < 50 && $pocion == 2){
+        $danio = $danios[$ataque] * 2;
+    }
+    else if($ataque = "golpe" && $pocion == 0){
+        $danio = $danios[$ataque] * 3;
+    }
+    else if($ataque = "patada" && pocion == 1){
+        $danio = $danios[$ataque] * 3;
+    }
+    return $danio;
+}
+
+function calcularVida($vida, $ataque, $pocion){
+    $danio = calcularDanio($vida, $ataque, $pocion);
+    $vida -= $danio;
+    return $vida;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+    include_once("./calculadora.php");
+?>
+ 
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Calculadora de daño</title>
+</head>
+<body>
+    <h2>Calculadora de daño</h2>
+    <form action="./index.php" method="POST">
+        <label for = "vida">Vida actual: </label>
+        <input type = "number" name = "vida"><br>
+ 
+        <label for = "Ataque"> Seleccione el tipo de ataque: </label>
+        <select name = "Ataque"> <br>
+            <option value = "golpe">Golpe </option>
+            <option value = "patada">Patada</option>
+            <option value = "espadazo">Espadazo</option>
+        </select>
+ 
+        <br><label for = "pocion" >Seleccione el tipo de pocion: </label> <br>
+        <input type = "radio" name = "pocion" value = 0> Poción de veneno. <br>
+        <input type = "radio" name = "pocion" value = 1> Poción de fuego. <br>
+        <input type = "radio" name = "pocion" value = 2> Poción de adormecimiento. <br>
+
+        <br><input type="submit" value="Calcular">
+    </form>
+    <?php
+        if(isset($_POST["ataque"])){
+        }
+    ?>
+</body>
+</html>
+ 
